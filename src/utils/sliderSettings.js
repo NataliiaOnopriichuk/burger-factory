c9b9b065188af052a6fbe6abc349ece96edfc333
DemoNataliia <nataliam110388@gmail.com>
2023-03-31 14:07:39 +0300

    addad tablet2

--- src/utils/sliderSettings.js
+++ src/utils/sliderSettings.js
@@ -7,14 +7,22 @@ export const settings = {
     dots: false,
     responsive: [
         {
-            breakpoint: 1025,
+            breakpoint: 1365,
             settings: {
                 slidesToShow: 3,
                 slidesToScroll: 1,
+                initialSlide: 0,
             },
         },
         {
-            breakpoint: 688,
+            breakpoint: 960,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            },
+        },
+        {
+            breakpoint: 768,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
